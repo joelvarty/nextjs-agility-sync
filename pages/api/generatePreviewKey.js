de25b547c21dcc05c2c89cdeb1f543090c57cfd1
Joel Varty <joelvarty@outlook.com>
2020-06-24 17:26:28 -0400

    fixed refs

--- pages/api/generatePreviewKey.js
+++ pages/api/generatePreviewKey.js
@@ -1,12 +1,12 @@
-import { generatePreviewKey } from '../../agility.node'
+import { validateSlugForPreview } from '../../agility/agility.node'
 
 export default async (req, res) => {
 
-    //TODO: Only generate the preview link if you are already in Preview!
-    //how can I check if i'm in preview mode already?
-    
-    const previewKey = generatePreviewKey();
+	//TODO: Only generate the preview link if you are already in Preview!
+	//how can I check if i'm in preview mode already?
 
-    //Return a valid preview key
-    res.end(previewKey)
+	const previewKey = generatePreviewKey();
+
+	//Return a valid preview key
+	res.end(previewKey)
 }
